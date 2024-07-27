14f2e6b034c167ed60e049fa9d762c1ccf5f3910
thanhntse <nguyenthaithanh07@gmail.com>
2024-07-28 00:22:51 +0700

    feat: init project

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig(() => {
+  return {
+    build: {
+      outDir: 'build',
+    },
+    plugins: [react()],
+    server: {
+      port: 3000,
+    },
+  };
+});
