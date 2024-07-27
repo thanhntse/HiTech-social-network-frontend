171d62c8f4cf7f0c6a005bfb57eb536400c3aeb2
thanhntse <nguyenthaithanh07@gmail.com>
2024-07-28 01:37:24 +0700

    feat: add sketch

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,13 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  important: '#root',
   theme: {
     extend: {},
+  },
+  corePlugins: {
+    // Remove the Tailwind CSS preflight styles so it can use Material UI's preflight instead (CssBaseline).
+    preflight: false,
   },
   plugins: [],
 }
