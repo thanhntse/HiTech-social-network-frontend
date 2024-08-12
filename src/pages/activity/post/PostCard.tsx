b5ae498c15879a9c1d877ef4d7db8697e3a5753f
thanhntse <nguyenthaithanh07@gmail.com>
2024-08-12 18:52:51 +0700

    feat: add post

--- src/pages/activity/post/PostCard.tsx
+++ src/pages/activity/post/PostCard.tsx
@@ -1,17 +1,83 @@
 import {
-  Box
+  Avatar,
+  Box,
+  IconButton,
+  Typography
 } from '@mui/material'
 import React from 'react'
+import { Image, Posts } from '../../../types';
+import {
+  Ellipsis,
+  X
+} from 'lucide-react';
 
-type Props = {}
-
-export default function PostCard({}: Props) {
+export default function PostCard({
+  id,
+  fullName,
+  avatar,
+  images,
+  content,
+  like,
+  commentCount
+}: Posts) {
   return (
     <>
       <Box
-        className={`w-full bg-white dark:bg-bg-secondary-dark h-[500px] rounded-lg flex justify-center items-center shadow-gray-200 dark:shadow-bg-secondary-dark shadow-md`}
+        className={`w-full bg-white dark:bg-bg-secondary-dark rounded-lg flex flex-col gap-3 shadow-gray-200 dark:shadow-bg-secondary-dark shadow-md p-8`}
       >
-        PostCard
+        <Box
+          className={`w-full flex justify-between items-start`}
+        >
+          <Box
+            className={`flex gap-4 items-center`}
+          >
+            <Avatar
+              alt={`Avatar ${fullName}`}
+              src={avatar.url}
+              className="w-16 h-16"
+            />
+            <Box
+              className={`w-full`}
+            >
+              <Typography
+                className='font-semibold text-lg hover:text-primary dark:hover:text-primary-dark transition-all ease-in-out duration-300 cursor-pointer'
+              >
+                {fullName}
+              </Typography>
+              <Typography
+                className='text-xs font-medium text-txt-primary-light dark:text-txt-primary-dark'
+              >
+                1 hour ago
+              </Typography>
+            </Box>
+          </Box>
+          <Box
+            className={``}
+          >
+            <IconButton
+              className='text-txt-primary-light dark:text-txt-primary-dark hover:bg-primary-light dark:hover:bg-bg-primary-dark hover:text-primary dark:hover:text-primary-dark'
+            >
+              <Ellipsis />
+            </IconButton>
+            <IconButton
+              className='text-txt-primary-light dark:text-txt-primary-dark hover:bg-primary-light dark:hover:bg-bg-primary-dark hover:text-primary dark:hover:text-primary-dark'
+            >
+              <X />
+            </IconButton>
+          </Box>
+        </Box>
+        <Box
+          className=''
+        >
+          <Typography>
+            <div dangerouslySetInnerHTML={{ __html: content }} />
+          </Typography>
+        </Box>
+        <Box
+          className='w-full'
+        >
+          <hr className='opacity-20'/>
+        </Box>
       </Box>
     </>
   )
--- src/pages/activity/post/PostList.tsx
+++ src/pages/activity/post/PostList.tsx
@@ -1,23 +1,60 @@
 import {
-  Box
+  Box,
+  Typography
 } from '@mui/material'
 import React from 'react'
 import PostCard from './PostCard';
+import RenderIf from '../../../components/RenderIf';
+import posts from '../../../constant/demo-posts-list';
+import { SearchX } from 'lucide-react';
+import { useTranslation } from 'react-i18next';
 
 type Props = {}
 
 export default function PostList({}: Props) {
+  const { t } = useTranslation();
   return (
     <>
       <Box
         className='w-full flex flex-col justify-center items-center gap-8'
       >
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
+
+        <RenderIf
+          ifTrue={posts.length > 0}
+        >
+          <>
+            {posts.map((post, index) => (
+              <PostCard
+                key={index}
+                id={post.id}
+                avatar={post.avatar}
+                fullName={post.fullName}
+                commentCount={post.commentCount}
+                content={post.content}
+                images={post.images}
+                like={post.like}
+              />
+            ))}
+          </>
+        </RenderIf>
+
+        <RenderIf
+          ifTrue={posts.length === 0}
+        >
+          <Box
+            className='flex gap-4 h-[300px] justify-center items-center'
+          >
+            <SearchX
+              className='w-10 h-10'
+            />
+            <Typography
+              className='text-3xl font-medium italic'
+            >
+              {t('noPostFound')}
+            </Typography>
+          </Box>
+        </RenderIf>
+
       </Box>
     </>
   );
